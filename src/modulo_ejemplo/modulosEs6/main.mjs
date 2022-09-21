e11e971b1fad519899bb9c28e3b925a5da2db68f
Jose David <jose7312000@gmail.com>
2022-09-21 15:00:22 -0500

    Nueva version de exportar y importar modulos

---
+++ src/modulo_ejemplo/modulosEs6/main.mjs
@@ -0,0 +1,6 @@
+// Y luego, en el index.mjs lo tengo de esta manera:
+import { greeting, prop1 } from "./modulo.mjs";
+
+greeting();
+console.log(prop1);
+// Me importo las propiedades y/o funciones de mi modulo y las uso.
---
+++ src/modulo_ejemplo/modulosEs6/modulo.mjs
@@ -0,0 +1,7 @@
+// Yo por mi parte lo hice así en el modulo.mjs:
+export function greeting() {
+	console.log('Hello world!');
+}
+
+export const prop1 = 'Hi, how are you?';
+// Lo que hago, es exportar tanto la función como la constante, justo en el momento de la declaración.
