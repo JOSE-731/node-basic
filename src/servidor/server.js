826a6f1d7b4674602973d57eb32a28887aa61d47
Jose David <jose7312000@gmail.com>
2022-09-22 11:23:13 -0500

    Recibiendo peticiones

--- src/servidor/server.js
+++ src/servidor/server.js
@@ -1,9 +1,10 @@
 //Para crear un servidor con expres debemos instalarlo con npm i express
 const express = require('express'); //Exportamos express
+const bodyParser = require('body-parser');//Body parser:Es un modulo que nos permite trabajar de forma mas sencilla con el body de la aplicacion
 const router = express.Router(); //Añadiendo el router a la aplicacion de express
-
-var app = express(); //Instanciamos la exportacion
-
+//npm i body-parser
+var app = express(); //Instanciamos la exportacion de express
+app.use(express.json());
 app.use(router);
 /*app.use('/', function(req, res){ //Definimos la ruta que se activará cuando sea ejecute
     res.send("Hola"); //Damos como respuesta un Hola cada que se consultada la ruta
@@ -11,17 +12,16 @@ app.use(router);
 
 router.get('/', function(req, res){
     res.send("Listando mensajes")
-    console.log("Server corriendo")
 })
 
 router.post('/menssage', function(req, res){
     res.send("Agregando mensaje")
+    console.log(`Request ${req}`);
 })
 
 router.delete('/delete', function(req, res){
     res.send("Eliminando mensaje")
 })
 
-
-
+console.log("Server corriendo")
 app.listen(3000);//Especificamos el puerto
