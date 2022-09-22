de20adccd229543472a417c0d75c3a63e0a9b52c
Jose David <jose7312000@gmail.com>
2022-09-21 21:31:11 -0500

    Agregando tipos de rutas

--- src/servidor/server.js
+++ src/servidor/server.js
@@ -1,11 +1,27 @@
 //Para crear un servidor con expres debemos instalarlo con npm i express
 const express = require('express'); //Exportamos express
+const router = express.Router(); //Añadiendo el router a la aplicacion de express
 
 var app = express(); //Instanciamos la exportacion
 
-app.use('/', function(req, res){ //Definimos la ruta 
+app.use(router);
+/*app.use('/', function(req, res){ //Definimos la ruta que se activará cuando sea ejecute
     res.send("Hola"); //Damos como respuesta un Hola cada que se consultada la ruta
+})*/
+
+router.get('/', function(req, res){
+    res.send("Listando mensajes")
+    console.log("Server corriendo")
 })
 
+router.post('/menssage', function(req, res){
+    res.send("Agregando mensaje")
+})
+
+router.delete('/delete', function(req, res){
+    res.send("Eliminando mensaje")
+})
+
+
+
 app.listen(3000);//Especificamos el puerto
-console.log("Server corriendo")
