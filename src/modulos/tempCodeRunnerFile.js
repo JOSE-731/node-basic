19116e00e475009487ab02ae9653ce3bd130d708
Jose David <jose7312000@gmail.com>
2022-10-04 21:28:07 -0500

    Obteniendo data con metodo post

--- src/modulos/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const { spawn } = require('child_process')
-const myprocess = spawn('dir')
-
-process.stdout.on("data", data => console.log(data.toString()));
-process.on("exit", () => console.log("process end"));
--- src/servidor/server.js
+++ src/servidor/server.js
@@ -1,11 +1,12 @@
 //Para crear un servidor con expres debemos instalarlo con npm i express
 const express = require('express'); //Exportamos express
-const bodyParser = require('body-parser');//Body parser:Es un modulo que nos permite trabajar de forma mas sencilla con el body de la aplicacion
 const router = express.Router(); //Añadiendo el router a la aplicacion de express
+const bodyParser = require('body-parser');//Body parser:Es un modulo que nos permite trabajar de forma mas sencilla con el body de la aplicacion
 //npm i body-parser
-var app = express(); //Instanciamos la exportacion de express
-app.use(express.json());
+const  app = express(); //Instanciamos la exportacion de express
+app.use(bodyParser.json())//Metodo para analizar las solicitudes entrantes
 app.use(router);
+
 /*app.use('/', function(req, res){ //Definimos la ruta que se activará cuando sea ejecute
     res.send("Hola"); //Damos como respuesta un Hola cada que se consultada la ruta
 })*/
@@ -16,7 +17,7 @@ router.get('/', function(req, res){
 
 router.post('/menssage', function(req, res){
     res.send("Agregando mensaje")
-    console.log(`Request ${req}`);
+    console.log(`Request ${req.body.test}`);
 })
 
 router.delete('/delete', function(req, res){
